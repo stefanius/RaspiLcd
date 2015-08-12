12a672ab195c0f7c538a4563b1b7dbec085ae698
stefanius <stefanius@users.noreply.github.com>
2015-08-12 22:46:57 +0200

    Update and rename print to print.py

---
+++ print.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from lib import *
+from subprocess import *
+from time import sleep, strftime
+from datetime import datetime
+
+lcd = Adafruit_CharLCD()
+
+cmd = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+
+lcd.begin(16, 1)
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+def run_cmd(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output
+
+lcd.clear()
+ipaddr = run_cmd(cmd)
+lcd.message('hello')
+lcd.message('world')
